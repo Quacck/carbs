d6575b47e4e8798ae48109fa5bd248ffb7dee3aa
Vincent <vincopi@googlemail.com>
2024-10-02 00:07:47 +0200

    Eval stuff

--- jobs/job-135.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export GUROBI_HOME="/home/vincent.opitz/master-thesis/gurobi1103/linux64"
-export LD_LIBRARY_PATH=$GUROBI_HOME/lib
-export PATH=$GUROBI_HOME/bin:$PATH
-export GRB_LICENSE_FILE="/home/vincent.opitz/master-thesis/gurobi1103/gurobi.lic"
-python3 src/run.py --scheduling-policy carbon --task-trace different_lengths --dynamic-power-draw --dynamic-power-draw-type constant-from-periodic-phases --dynamic-power-draw-phases "{'startup':[{'name': 'startup','duration': 0, 'power': 150}],'work':[{'name': 'high', 'power': 200, 'duration': 3600}, {'name': 'low', 'power': 100, 'duration': 1800}]}" --carbon-policy oracle --start-index 7000 --w 24 --filename results/simulation/different_lengths/carbon_constant-from-periodic-phases_1_0_200_24 --repeat
